e12c02a14b03e79907260b83ab2315442b56b879
Lexi Oliveira <lexi.m.oliveira@gmail.com>
2025-09-27 18:49:40 -0300

    Initial Commit

---
+++ ConvX/ConvXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvXApp.swift
+//  ConvX
+//
+//  Created by Lexi Oliveira on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ConvXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
